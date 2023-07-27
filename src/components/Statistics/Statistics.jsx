f125f69be79fd7b770d4e099cd64b6a1aed0452a
Darya Hudzovska <102170750+bestboost@users.noreply.github.com>
2023-07-27 15:57:15 +0300

    hook statistics

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,50 +1,50 @@
-import PropTypes from 'prop-types';
-import React, {Component} from 'react';
+// import PropTypes from 'prop-types';
+import React from 'react';
 import {StatisticsTitle, FeedbackList, FeedbackItem, FeedbackName, Statistic} from './Statistics.styled';
 import { Box } from '../Box';
 
-class Feedback extends Component {
-  static propTypes = {
-        good: PropTypes.number.isRequired,
-        neutral: PropTypes.number.isRequired,
-        bad: PropTypes.number.isRequired,
-        total: PropTypes.number.isRequired,
-        positivePercentage: PropTypes.number.isRequired,
-  };
+const Feedback = ({good, neutral, bad, total, positivePercentage}) => {
+//   static propTypes = {
+//         good: PropTypes.number.isRequired,
+//         neutral: PropTypes.number.isRequired,
+//         bad: PropTypes.number.isRequired,
+//         total: PropTypes.number.isRequired,
+//         positivePercentage: PropTypes.number.isRequired,
+//   };
 
 
-    render() { 
-const {good, neutral, bad, total, positivePercentage} = this.props
+    // render() { 
+//  const {good, neutral, bad, total, positivePercentage} = this.props
 
-        return (
-            <Box>
-              <StatisticsTitle>Statistics</StatisticsTitle>
-                 <FeedbackList>
-                    <FeedbackItem>
-                        <FeedbackName >Good:</FeedbackName > 
-                        <Statistic>{good}</Statistic>
-                    </FeedbackItem>
-                    <FeedbackItem>
-                        <FeedbackName >Neutral:</FeedbackName >
-                        <Statistic>{neutral}</Statistic>
-                    </FeedbackItem>
-                    <FeedbackItem>
-                        <FeedbackName >Bad:</FeedbackName >
-                        <Statistic>{bad}</Statistic>
-                    </FeedbackItem>    
-                    <FeedbackItem>
-                        <FeedbackName >Total:</FeedbackName >
-                        <Statistic >{total}</Statistic>
-                    </FeedbackItem>
-                    <FeedbackItem>
-                        <FeedbackName >Positive feedback:</FeedbackName >
-                        <Statistic>{positivePercentage}%</Statistic>
-                    </FeedbackItem>
-                 </FeedbackList>
-           
-           </Box>
-        );
-    }
+    return (
+        <Box>
+            <StatisticsTitle>Statistics</StatisticsTitle>
+                <FeedbackList>
+                <FeedbackItem>
+                    <FeedbackName >Good:</FeedbackName > 
+                    <Statistic>{good}</Statistic>
+                </FeedbackItem>
+                <FeedbackItem>
+                    <FeedbackName >Neutral:</FeedbackName >
+                    <Statistic>{neutral}</Statistic>
+                </FeedbackItem>
+                <FeedbackItem>
+                    <FeedbackName >Bad:</FeedbackName >
+                    <Statistic>{bad}</Statistic>
+                </FeedbackItem>    
+                <FeedbackItem>
+                    <FeedbackName >Total:</FeedbackName >
+                    <Statistic >{total}</Statistic>
+                </FeedbackItem>
+                <FeedbackItem>
+                    <FeedbackName >Positive feedback:</FeedbackName >
+                    <Statistic>{positivePercentage}%</Statistic>
+                </FeedbackItem>
+                </FeedbackList>
+        
+        </Box>
+    );
 };
+// };
 
 export default Feedback;
